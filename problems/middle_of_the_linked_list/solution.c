1d7f57f646e1a97d9a31b3ecb684af54e93913cb
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2023-10-28 14:58:30 +0000

    Sync LeetCode submission - Middle of the Linked List (c)

---
+++ problems/middle_of_the_linked_list/solution.c
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+int count(struct ListNode* start){
+   struct ListNode *q;
+   q = start;
+   int c=0;
+   while (q!= NULL) {
+      c++;
+      q=q->next;
+   }
+   return c;
+}
+
+struct ListNode* delpos(struct ListNode* start, int pos) {
+   struct ListNode *q, *temp;
+   q=start;
+   for (int c=0; c<pos-2; c++)
+      q=q-> next;
+   return q->next;
+}
+
+struct ListNode* middleNode(struct ListNode* head){
+   int c=count(head),s=0;
+   if (c==1)
+      return head;
+   else
+      return delpos(head,(c/2)+1);
+   // struct ListNode* q=malloc(sizeof(struct ListNode));
+   // q->val=s,q->next=NULL;
+   // return q;
+}
