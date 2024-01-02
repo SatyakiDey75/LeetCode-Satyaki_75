293d1e5e38ac7d0f6299cc4153de7b6c3bca70af
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-02 17:54:25 +0530

    Time: 3415 ms (7.25%) | Memory: 17.7 MB (13.56%) - LeetSync

---
+++ problems/1203-print-in-order/print-in-order.py
@@ -0,0 +1,28 @@
+class Foo:
+    f,s=False,False
+    def __init__(self):
+        pass
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.f=True
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        while not self.f:
+            continue
+        printSecond()
+        self.s=True
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        while not self.s:
+            continue
+        printThird()
