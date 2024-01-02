b90b33ee7014e4e0fb0c6c5e0f2998de0afb2c84
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-02 20:59:49 +0530

    Time: 32 ms (5.19%) | Memory: 13.3 MB (55.71%) - LeetSync

---
+++ problems/168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -0,0 +1,16 @@
+def let(i):
+    s='Z'
+    if i%26>0:
+        s=chr(64+(i%26))
+        c=i//26
+    return s
+
+class Solution(object):
+    def convertToTitle(self, c):
+        s=''
+        while c>0:
+            s+=let(c)
+            if c%26==0: c=(c//26)-1
+            else: c//=26
+            print(c,s)
+        return s[::-1]
