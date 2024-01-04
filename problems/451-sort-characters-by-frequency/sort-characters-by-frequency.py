0fb0b231dc5707701f84df573c5251d4dd5647e5
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-04 11:26:21 +0530

    Time: 26 ms (79.43%) | Memory: 15.8 MB (58.59%) - LeetSync

--- problems/451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ problems/451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def frequencySort(self, s):
-        all_freq={}
+        af={}
         res=""
         for i in s:
-            if i in all_freq:
-                all_freq[i]+=1
+            if i in af:
+                af[i]+=1
             else:
-                all_freq[i]=1
-        rl = sorted(all_freq.items(),key=lambda x:x[1],reverse=True)
+                af[i]=1
+        rl = sorted(af.items(),key=lambda x:x[1],reverse=True)
         for i in rl:
             res+=(i[0]*i[1])
         return res
