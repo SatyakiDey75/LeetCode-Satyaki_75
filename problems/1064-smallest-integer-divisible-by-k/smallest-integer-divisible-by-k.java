611bad149f3242bf773669e75457af4d33cb6042
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-04 11:53:08 +0530

    Time: 2 ms (89.89%) | Memory: 40.4 MB (30.34%) - LeetSync

---
+++ problems/1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        int r=0;
+        for (int l=1;l<=k;l++) {
+            r=(r*10+1)%k;
+            if (r==0)
+                return l;
+        }
+        return -1;
+    }
+}
