b529461e5e74ffe38dcbe856e208e1731aab567d
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-04 10:56:03 +0530

    Time: 742 ms (8.25%) | Memory: 13.5 MB (67.01%) - LeetSync

--- problems/2714-left-and-right-sum-differences/left-and-right-sum-differences.py
+++ problems/2714-left-and-right-sum-differences/left-and-right-sum-differences.py
@@ -5,17 +5,9 @@ class Solution(object):
         :rtype: List[int]
         """
         l=[]
-        if len(nums)==1:
-            return [0]
-        elif len(nums)==2:
-            return nums[::-1]
-        
-        else:
-            for i in range (0,len(nums)):
-                ls,rs=0,0
-                for j in range(0,i):
-                    ls+=nums[j]
-                for j in range(i+1,len(nums)):
-                    rs+=nums[j]
-                l.append(max(ls,rs)-min(ls,rs))
-            return l
+        for i in range (0,len(nums)):
+            ls,rs=0,0
+            for j in range(0,i):    ls+=nums[j]
+            for j in range(i+1,len(nums)):  rs+=nums[j]
+            l.append(max(ls,rs)-min(ls,rs))
+        return l
