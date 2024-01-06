46c3d1c9a48d4e295b0dc037ae8c192e7771ea30
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-06 20:02:32 +0530

    Time: 17 ms (65.64%) | Memory: 13.4 MB (48.21%) - LeetSync

--- problems/2406-decode-the-message/decode-the-message.py
+++ problems/2406-decode-the-message/decode-the-message.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def decodeMessage(self, key, message):
-        keyToActual = {' ': ' '}
-        currChar = 'a'
-
-        for c in key:
-            if c not in keyToActual:
-                keyToActual[c] = currChar
-                currChar = chr(ord(currChar) + 1)
-
-        return ''.join(keyToActual[c] for c in message)
+        dict_of_keys={" ":" "}
+        char="a"
+        for i in key:
+            if i not in dict_of_keys:
+                dict_of_keys[i]=char
+                char=chr(ord(char)+1)
+        return "".join(dict_of_keys[i] for i in message)
