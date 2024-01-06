6ca88a661f9f6841dda4c07c3f346f2955347fe9
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-06 20:01:16 +0530

    Time: 21 ms (46.67%) | Memory: 13.4 MB (48.21%) - LeetSync

--- problems/2406-decode-the-message/decode-the-message.py
+++ problems/2406-decode-the-message/decode-the-message.py
@@ -1,11 +1,19 @@
 class Solution(object):
     def decodeMessage(self, key, message):
-        alp=list("abcdefghijklmnopqrstuvwxyz")
-        mat= list(OrderedDict.fromkeys(list((key.replace(" ","")))))
-        s=""
-        for i in message:
-            if i==" ":
-                s+=i
-            else:
-                s+=alp[mat.index(i)]
-        return s
+        # alp=list("abcdefghijklmnopqrstuvwxyz")
+        # mat= list(OrderedDict.fromkeys(list((key.replace(" ","")))))
+        # s=""
+        # for i in message:
+        #     if i==" ":
+        #         s+=i
+        #     else:
+        #         s+=alp[mat.index(i)]
+        # return s
+        # more optimised:
+        dict_of_keys={" ":" "}
+        char="a"
+        for i in key:
+            if i not in dict_of_keys:
+                dict_of_keys[i]=char
+                char=chr(ord(char)+1)
+        return "".join(dict_of_keys[i] for i in message)
