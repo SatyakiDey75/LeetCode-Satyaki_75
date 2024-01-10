d1e9f781109a0146602b9b98772729366702adee
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-10 18:46:13 +0530

    Time: 6 ms (15.29%) | Memory: 6.2 MB (83.70%) - LeetSync

--- problems/29-divide-two-integers/divide-two-integers.c
+++ problems/29-divide-two-integers/divide-two-integers.c
@@ -1,13 +1,11 @@
 int divide(int dividend, int divisor) {
-    if (dividend == INT_MIN && divisor == -1){
+    if (dividend==INT_MIN && divisor==-1){
         return INT_MAX;
     }
-    long long int res = (dividend / divisor);
-    if (res < INT_MIN){
+    long long int res=(dividend/divisor);
+    if (res<INT_MIN)
         return INT_MIN;
-    }
-    else if (res > INT_MAX){
+    else if (res>INT_MAX)
         return INT_MAX;
-    }
     return res;
 }
