d97ec28ff8dbcb67119307ecfd94e8569f8fe7d4
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-10 18:37:07 +0530

    Time: 55 ms (29.58%) | Memory: 41.9 MB (31.44%) - LeetSync

---
+++ problems/2734-array-prototype-last/array-prototype-last.js
@@ -0,0 +1,13 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    var len=this.length;
+    if (len!=0) return this[len-1];
+    else    return -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
