b6e1207bba72d6c68ffb28be976fe8571df212e9
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-10 12:15:55 +0530

    Time: 41 ms (12.50%) | Memory: 13.3 MB (75.00%) - LeetSync

--- problems/1755-defuse-the-bomb/defuse-the-bomb.py
+++ problems/1755-defuse-the-bomb/defuse-the-bomb.py
@@ -4,17 +4,10 @@ class Solution(object):
         if k==0:
             return c
         for i in range (len(code)):
-            s,n,b=0,i+1,k
-            if k<0:
-                n=i-1
-                b*=(-1)
-            for j in range (b):
-                print(s,code[n%len(code)],n%len(code))
-                s+=code[n%len(code)]
-                if k<0:
-                    n-=1
-                else:    
-                    n+=1
-            c[i]=s
-
+            if k>0:
+                for j in range (i+1,i+k+1):
+                    c[i]+=code[j%len(code)]
+            else:
+                for j in range (i+k,i):
+                    c[i]+=code[(j+len(code))%len(code)]
         return c
