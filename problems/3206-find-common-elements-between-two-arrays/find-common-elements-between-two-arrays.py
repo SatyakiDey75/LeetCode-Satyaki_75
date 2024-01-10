e50c1ef102002b6bd0a386ffd63d988ce8783c5a
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-10 19:07:31 +0530

    Time: 126 ms (38.67%) | Memory: 13.2 MB (97.04%) - LeetSync

--- problems/3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.py
+++ problems/3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def findIntersectionValues(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
         min,max=(list(set(nums1)),list(set(nums2))) if len(nums1)<len(nums2) else (list(set(nums2)),list(set(nums1)))
         l=[0]*2
         for i in min:
