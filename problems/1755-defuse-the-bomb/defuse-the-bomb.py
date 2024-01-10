5a9ad343303579705ac634268d45fff6c4c068ff
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-10 12:27:50 +0530

    Time: 41 ms (12.50%) | Memory: 13.4 MB (17.19%) - LeetSync

--- problems/1755-defuse-the-bomb/defuse-the-bomb.py
+++ problems/1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,12 +1,13 @@
-class Solution:
-    def decrypt(self, code: List[int], k: int) -> List[int]:
+class Solution(object):
+    def decrypt(self, code, k):
         c=[0]*len(code)
         if k==0:
             return c
-        s=list(accumulate(code+code, initial=0))
         for i in range (len(code)):
             if k>0:
-                c[i]+=s[i+k+1]-s[i+1]
+                for j in range (i+1,i+k+1):
+                    c[i]+=code[j%len(code)]
             else:
-                c[i]+=s[i+len(code)]-s[i+k+len(code)]
+                for j in range (i+k,i):
+                    c[i]+=code[(j+len(code))%len(code)]
         return c
