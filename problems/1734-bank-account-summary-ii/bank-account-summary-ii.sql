60682bd4ca6f67ebc0415f0c5d37f9f20bd720f7
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-11 11:54:57 +0530

    Time: 1557 ms (44.72%) | Memory: 0B (100.00%) - LeetSync

--- problems/1734-bank-account-summary-ii/bank-account-summary-ii.sql
+++ problems/1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select c.name, c.balance from (select a.name, a.account, sum(t.amount) as balance from users a left join transactions t on a.account=t.account group by a.account) c where c.balance>10000;
+select c.name, c.balance from (select a.name, a.account, sum(t.amount) as balance from users a left join transactions t on a.account=t.account group by a.account having balance>10000) c;
