da7ea67d2c0af8f35b93602b3a212bd0d7646d42
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-11 11:36:25 +0530

    Time: 5 ms (29.51%) | Memory: 6.2 MB (92.70%) - LeetSync

--- problems/7-reverse-integer/reverse-integer.c
+++ problems/7-reverse-integer/reverse-integer.c
@@ -2,10 +2,11 @@ int rev(int x, int y) {
     long s=0;
     while (x>0) {
         s=(s*10)+(x%10);
+        if (s>INT_MAX || s<INT_MIN)
+            return 0;
         x=(int)x/10;
     }
-    if (s>INT_MAX || s<INT_MIN)
-        return 0;
+    
     return s*y;
 }
 
