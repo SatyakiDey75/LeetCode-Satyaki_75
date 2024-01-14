9732b501f23d4f051a0955b6ce7824c50cc8cd16
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-14 22:51:40 +0530

    Time: 90 ms (5.03%) | Memory: 24.8 MB (5.51%) - LeetSync

--- problems/898-transpose-matrix/transpose-matrix.py
+++ problems/898-transpose-matrix/transpose-matrix.py
@@ -1,13 +1,13 @@
 import numpy
 class Solution(object):
     def transpose(self, m):
-        c=len(m[0])
-        r=len(m)
-        tr=[]
-        for i in range (c):
-            l=[]
-            for j in range (r):
-                l.append(m[j][i])
-            tr.append(l)
-        return tr
+        row = len(m)
+        col = len(m[0])
+        res = []
+        for i in range(col):
+            temp = []
+            for j in range(row):
+                temp.append(m[j][i])
+            res.append(temp)
+        return res
         
