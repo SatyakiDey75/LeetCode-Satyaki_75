a6934202b1dad6570d3db925b72cc7c41ab4d737
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-14 23:04:02 +0530

    Time: 27 ms (79.36%) | Memory: 13.4 MB (58.30%) - LeetSync

--- problems/728-self-dividing-numbers/self-dividing-numbers.py
+++ problems/728-self-dividing-numbers/self-dividing-numbers.py
@@ -1,7 +1,7 @@
 def sdn(n):
     b=n
     while b>0:
-        if n%(b%10)!=0:
+        if b%10==0 or n%(b%10)!=0:
             return 0
         b//=10
     return 1
@@ -10,6 +10,6 @@ class Solution(object):
     def selfDividingNumbers(self, l, r):
         li=[]
         for i in range (l,r+1):
-            if '0' not in str(i) and sdn(i)==1:
+            if sdn(i)==1:
                 li.append(i)
         return li
