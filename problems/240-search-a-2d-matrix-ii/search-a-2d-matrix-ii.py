895149c39b6b756f60e60b0509b311c10a728def
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-15 00:54:24 +0530

    Time: 117 ms (87.17%) | Memory: 19.3 MB (96.84%) - LeetSync

--- problems/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ problems/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -1,21 +1,16 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        f=0
-        for i in range (0,len(matrix)):
-            if target<matrix[i][len(matrix[i])-1]:
-                j=0
-                while (j<len(matrix[i])-1):
-                    if matrix[i][j]==target:
-                        f=1
-                        return True
-                    else:
-                        j+=1
-            elif target==matrix[i][len(matrix[i])-1]:
-                f=1
+        r=0
+        c=len(matrix[0])-1
+
+        while r<len(matrix) and c>=0:
+            if matrix[r][c]==target:
                 return True
+            if target<matrix[r][c]:
+                c-=1
             else:
-                continue
-        if f==0:
-            return False
+                r+=1
+
+        return False
+                
             
-        
