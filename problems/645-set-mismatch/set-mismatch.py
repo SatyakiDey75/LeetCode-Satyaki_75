069e9efa6256ff430d91c6a246e29599887b3621
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-16 02:14:36 +0530

    Time: 156 ms (48.80%) | Memory: 14.4 MB (95.22%) - LeetSync

---
+++ problems/645-set-mismatch/set-mismatch.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findErrorNums(self, nums):
+        for i in nums:
+            if nums[abs(i)-1]<0:
+                d=abs(i)
+            else:
+                nums[abs(i)-1]*=-1
+        for i, j in enumerate(nums):
+            if j>0:
+                return [d,i+1]
+            
