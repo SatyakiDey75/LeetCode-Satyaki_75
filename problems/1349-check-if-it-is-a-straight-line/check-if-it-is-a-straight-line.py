1245950534d3fb00d7facb629f2785346ef0117a
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-15 22:40:29 +0530

    Time: 34 ms (85.43%) | Memory: 13.7 MB (98.01%) - LeetSync

--- problems/1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
+++ problems/1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
@@ -2,11 +2,10 @@ class Solution(object):
     def checkStraightLine(self, c):
         if len(c)==2:
             return True
-        else:
-            a=-(c[1][1]-c[0][1])
-            b=(c[1][0]-c[0][0])
-            c1=-a*c[0][0]-b*c[0][1]
-            for i in range(2,len(c)):
-                if (a*c[i][0]+b*c[i][1]+c1)!=0:
-                    return False
-            return True
+        a=-(c[1][1]-c[0][1])
+        b=(c[1][0]-c[0][0])
+        c1=-a*c[0][0]-b*c[0][1]
+        for i in range(2,len(c)):
+            if (a*c[i][0]+b*c[i][1]+c1)!=0:
+                return False
+        return True
