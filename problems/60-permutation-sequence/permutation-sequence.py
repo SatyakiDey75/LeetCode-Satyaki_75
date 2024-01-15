e55a74d02d4578d3707f537f3f78509c8ad9bcca
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-15 19:42:40 +0530

    Time: 10 ms (95.86%) | Memory: 13.1 MB (98.52%) - LeetSync

--- problems/60-permutation-sequence/permutation-sequence.py
+++ problems/60-permutation-sequence/permutation-sequence.py
@@ -1,8 +1,14 @@
 class Solution(object):
     def getPermutation(self, n, k):
-        l=[i for i in range (1,n+1)]
-        r=list(itertools.permutations(l))[k-1]
-        s=""
-        for i in r:
-            s+=str(i)
-        return s
+        r=""
+        nu=[i+1 for i in range(n)]
+        f=[1]*(n+1) 
+        for i in range(2,n+1):
+            f[i]=f[i-1]*i
+        k-=1
+        for i in range(n-1,-1,-1):
+            j=k//f[i]
+            k%=f[i]
+            r+=str(nu[j])
+            nu.pop(j)
+        return r
