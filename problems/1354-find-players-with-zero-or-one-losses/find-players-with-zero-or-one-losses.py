2cbb9ce5427e259038dd2a77f24839181c0d02eb
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-15 17:45:58 +0530

    Time: 1389 ms (68.87%) | Memory: 72.3 MB (83.27%) - LeetSync

--- problems/1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ problems/1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -1,12 +1,17 @@
 class Solution(object):
     def findWinners(self, matches):
-        ans=[[] for i in range(2)]
-        lc=collections.Counter()
-        for i,j in matches:
-            if i not in lc:
-                lc[i]=0
-            lc[j]+=1
-        for p,n in lc.items():
-            if n<2:
-                ans[n].append(p)
-        return [sorted(ans[0]),sorted(ans[1])]
+        z_l,o_l,m_l=set(),set(),set()
+        for w,l in matches:
+            if w not in o_l and w not in m_l:
+                z_l.add(w)
+            if l in z_l:
+                z_l.remove(l)
+                o_l.add(l)
+            elif l in o_l:
+                o_l.remove(l)
+                m_l.add(l)
+            elif l in m_l:
+                continue
+            else:
+                o_l.add(l)          
+        return [sorted(list(z_l)), sorted(list(o_l))]
