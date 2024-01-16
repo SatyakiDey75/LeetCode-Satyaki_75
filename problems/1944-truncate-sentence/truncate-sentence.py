0711384161fd9ee331e4ae979c222533855cd008
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-16 17:27:32 +0530

    Time: 14 ms (64.71%) | Memory: 13.6 MB (13.60%) - LeetSync

--- problems/1944-truncate-sentence/truncate-sentence.py
+++ problems/1944-truncate-sentence/truncate-sentence.py
@@ -1,5 +1,12 @@
 class Solution(object):
     def truncateSentence(self, s, k):
-        l=s.split()
-        return ' '.join(l[:k])
+        x=""
+        d=0
+        for i in s.split(" "):
+            d+=1
+            if(d<k):
+                x+=i+" "
+            elif(d==k):
+                x+=i
+        return x
         
