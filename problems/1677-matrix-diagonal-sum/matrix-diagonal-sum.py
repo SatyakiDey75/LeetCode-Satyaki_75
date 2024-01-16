f12cc6e5d5c2197c2939438c1f8f199e11724cea
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-16 15:25:24 +0530

    Time: 76 ms (73.45%) | Memory: 13.3 MB (94.08%) - LeetSync

--- problems/1677-matrix-diagonal-sum/matrix-diagonal-sum.py
+++ problems/1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -2,7 +2,7 @@ class Solution(object):
     def diagonalSum(self, mat):
         s=0
         for i in range (len(mat)):
-            for j in range (len(mat[0])):
+            for j in range (len(mat)):
                 if i==j or i+j==len(mat)-1:
                     s+=mat[i][j]
         return s
