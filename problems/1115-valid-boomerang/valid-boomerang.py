237dba86f1979f35ded18cda961c4c9b2185b3c5
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-16 20:12:27 +0530

    Time: 13 ms (81.54%) | Memory: 13.4 MB (27.69%) - LeetSync

--- problems/1115-valid-boomerang/valid-boomerang.py
+++ problems/1115-valid-boomerang/valid-boomerang.py
@@ -2,12 +2,7 @@ class Solution(object):
     def isBoomerang(self, p):
         if p[1]==p[2] or p[2]==p[0] or p[0]==p[1]:
             return False
-        c=p
-        a=-(c[1][1]-c[0][1])
-        b=(c[1][0]-c[0][0])
-        c1=-a*c[0][0]-b*c[0][1]
-        for i in range(2,len(c)):
-            if (a*c[i][0]+b*c[i][1]+c1)!=0:
-                return True
-        return False
-        
+        x1,y1=p[0][0],p[0][1]
+        x2,y2=p[1][0],p[1][1]
+        x3,y3=p[2][0],p[2][1]
+        return (y3-y2)*(x2-x1)!=(y2-y1)*(x3-x2)
