310922af2e94c99a106eac341f6484625b40ac7b
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-17 12:42:35 +0530

    Time: 3 ms (54.24%) | Memory: 6.2 MB (81.36%) - LeetSync

---
+++ problems/492-construct-the-rectangle/construct-the-rectangle.c
@@ -0,0 +1,14 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdlib.h>
+int* constructRectangle(int area, int* returnSize) {
+    int j=(int)sqrt(area);
+    while(area%j!=0)
+        j-=1;
+    int* a=(int*)malloc(2*sizeof(int));
+    a[0]=area/j;
+    a[1]=j;
+    *returnSize=2;
+    return a;
+}
