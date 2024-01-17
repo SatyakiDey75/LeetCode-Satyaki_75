097484b5830122a674556d7bd4a0bc42a2b65ba8
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-17 13:55:30 +0530

    Time: 28 ms (68.62%) | Memory: 13.5 MB (23.26%) - LeetSync

--- problems/2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++ problems/2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -2,7 +2,7 @@ class Solution(object):
     def finalValueAfterOperations(self, ops):
         x=0
         for i in ops:
-            if i in ("++X","X++"):
+            if i=="X++" or i=="++X":
                 x+=1
             else:
                 x-=1
