efe346cff8ed7fab31db26868d27c69b520546fb
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-17 10:51:29 +0530

    Time: 119 ms (91.91%) | Memory: 20.3 MB (93.06%) - LeetSync

--- problems/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ problems/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,3 +1,18 @@
 class Solution(object):
     def singleNonDuplicate(self, nums):
-        return Counter(nums).most_common()[-1][0]
+        n=len(nums)
+        l=0
+        r=n-1
+        while(l<r):
+            m=(l+r)//2
+            if (m+1)%2:
+                if nums[m+1]==nums[m]:
+                    l=m+2
+                else:
+                    r=m
+            else:
+                if nums[m-1]==nums[m]:
+                    l=m+1
+                else:
+                    r=m-1
+        return nums[r]
