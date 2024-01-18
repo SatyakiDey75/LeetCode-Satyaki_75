59a08cc9757d59293375b657f807ecf9bf0e13c4
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-18 11:10:16 +0530

    Time: 30 ms (93.43%) | Memory: 13.4 MB (33.14%) - LeetSync

--- problems/3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.py
+++ problems/3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.py
@@ -1,8 +1,17 @@
 class Solution(object):
     def countTestedDevices(self, bp):
+
+        # Naive Method:
+        # c=0
+        # for i in range (len(bp)):
+        #     if bp[i]>0:
+        #         c+=1
+        #         bp[i+1:]=[max(0,bp[j]-1) for j in range (i+1,len(bp))]
+        # return c
+
+        # Shortcut Method:
         c=0
         for i in range (len(bp)):
-            if bp[i]>0:
+            if bp[i]-c>0:
                 c+=1
-                bp[i+1:]=[max(0,bp[j]-1) for j in range (i+1,len(bp))]
         return c
