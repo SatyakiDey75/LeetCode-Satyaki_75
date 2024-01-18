2eb693369cb558611b0d7e25bba54bde2f060221
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-19 00:06:05 +0530

    Time: 35 ms (55.13%) | Memory: 20.3 MB (83.32%) - LeetSync

--- problems/141-linked-list-cycle/linked-list-cycle.py
+++ problems/141-linked-list-cycle/linked-list-cycle.py
@@ -5,7 +5,15 @@
 #         self.next = None
 
 class Solution(object):
-    def hasCycle(self,head):
+    def hasCycle(self, head):
+        # r=[]
+        # q=head
+        # while q not in r and q!=None:
+        #     r.append(q)
+        #     q=q.next
+        # return q!=None
+
+        # 100% beats:
         i,j=head,head
         while j and j.next:
             i=i.next
