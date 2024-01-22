0d9954df35aa54f5629a01154e59ecb8416df115
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-22 20:58:15 +0530

    Time: 73 ms (98.01%) | Memory: 12.8 MB (99.57%) - LeetSync

--- problems/229-majority-element-ii/majority-element-ii.py
+++ problems/229-majority-element-ii/majority-element-ii.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def majorityElement(self, nums):
-        l=[]
-        for i in nums:
-            if nums.count(i)>len(nums)/3 and i not in l:
+        l,s=[],len(nums)//3
+        for i in set(nums):
+            if nums.count(i)>s:
                 l.append(i)
         return l        
