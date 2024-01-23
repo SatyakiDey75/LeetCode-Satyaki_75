a0d4559aa9b1a9f0a67bce0351764285d7765777
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-23 13:12:05 +0530

    Time: 60 ms (96.78%) | Memory: 19.4 MB (31.19%) - LeetSync

--- problems/1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ problems/1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -5,15 +5,15 @@ class Solution:
         for i in arr:
             m=0
             for ch in i:
-                ch_index=ord(ch) - ord('a')
-                if m >> ch_index & 1:
+                ch_i=ord(ch)-ord('a')
+                if m>>ch_i & 1:
                     m=0
                     break
-                m |= 1 << ch_index
-            if m != 0:
+                m|=1<<ch_i
+            if m!=0:
                 for e_m in ms.copy():
-                    if e_m & m == 0:
-                        n_m=e_m | m
+                    if e_m & m==0:
+                        n_m=e_m|m
                         ms.append(n_m)
                         ml=max(ml, bin(n_m).count('1'))
         return ml
