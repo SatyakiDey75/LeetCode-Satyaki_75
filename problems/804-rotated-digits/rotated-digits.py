2ca024f0e99037349ec45812d8635d897a706d68
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-23 19:55:34 +0530

    Time: 117 ms (15.73%) | Memory: 16.4 MB (63.79%) - LeetSync

--- problems/804-rotated-digits/rotated-digits.py
+++ problems/804-rotated-digits/rotated-digits.py
@@ -2,10 +2,9 @@ def rot(n):
     n1=list(str(n))
     d={'0':'0','1':'1','2':'5','5':'2','6':'9','8':'8','9':'6'}
     for i in range (len(n1)):
-        if n1[i] in d:
-            n1[i]=d[n1[i]]
-        else:
+        if n1[i] not in d:
             return n
+        n1[i]=d[n1[i]]
     s=0
     for i in n1:
         s=s*10+int(i)
