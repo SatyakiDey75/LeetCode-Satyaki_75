2665ed5f58b416c15fc59a261e9737bb44f7b6f4
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-23 19:53:37 +0530

    Time: 112 ms (17.24%) | Memory: 16.3 MB (76.08%) - LeetSync

---
+++ problems/804-rotated-digits/rotated-digits.py
@@ -0,0 +1,16 @@
+def rot(n):
+    n1=list(str(n))
+    d={'0':'0','1':'1','2':'5','5':'2','6':'9','8':'8','9':'6'}
+    for i in range (len(n1)):
+        if n1[i] in d:
+            n1[i]=d[n1[i]]
+        else:
+            return n
+    s=0
+    for i in n1:
+        s=s*10+int(i)
+    return s
+
+class Solution:
+    def rotatedDigits(self, n: int) -> int:
+        return sum(1 for i in range (1,n+1) if i!=rot(i))
