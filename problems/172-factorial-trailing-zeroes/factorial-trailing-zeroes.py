8453c8e1b26484d1155eb69d0fd91a206d6ec753
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-23 12:55:41 +0530

    Time: 14 ms (79.96%) | Memory: 11.5 MB (98.99%) - LeetSync

--- problems/172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ problems/172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -1,10 +1,13 @@
-def fact(h):
-    if h==0:
-        return 1
-    return h*fact(h-1)
+# def fact(h):
+#     if h==0:
+#         return 1
+#     return h*fact(h-1)
+
+# class Solution(object):
+#     def trailingZeroes(self, n):
+#         s=str(fact(n))
+#         return len(s)-len(s.rstrip('0'))
 
 class Solution(object):
     def trailingZeroes(self, n):
-        s=str(fact(n))
-        return len(s)-len(s.rstrip('0'))
-
+        return 0 if n==0 else n//5+self.trailingZeroes(n//5)
