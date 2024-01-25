472877caee3018f7b03ec948e56fce2aa989a59c
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-25 11:57:53 +0530

    Time: 6 ms (98.01%) | Memory: 11.7 MB (98.01%) - LeetSync

--- problems/165-compare-version-numbers/compare-version-numbers.py
+++ problems/165-compare-version-numbers/compare-version-numbers.py
@@ -1,18 +1,14 @@
 class Solution(object):
     def compareVersion(self, v1, v2):
-        r1=v1.split('.')
-        r2=v2.split('.')
-        if len(r1)>len(r2):
-            for i in range (len(r1)-len(r2)):
-                r2.append('0')
-        
-        elif len(r1)<len(r2):
-            for i in range (len(r2)-len(r1)):
-                r1.append('0')
-        for i in range (len(r1)):
-            if int(r1[i])>int(r2[i]):
+        r1=[int(i) for i in v1.split('.')]
+        r2=[int(i) for i in v2.split('.')]
+        ml=max(len(r1),len(r2))
+        r1.extend([0]*(ml-len(r1)))
+        r2.extend([0]*(ml-len(r2)))
+        for i in range (ml):
+            if r1[i]>r2[i]:
                 return 1
-            if int(r1[i])<int(r2[i]):
+            if r1[i]<r2[i]:
                 return -1
         return 0
         
