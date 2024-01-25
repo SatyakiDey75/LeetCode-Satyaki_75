83cec34c77ce21473f0e7969490a0810f51e277b
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-25 18:14:19 +0530

    Time: 30 ms (91.30%) | Memory: 16.5 MB (56.78%) - LeetSync

--- problems/1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.py
+++ problems/1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.py
@@ -2,6 +2,10 @@ class Solution:
     def sumBase(self, n: int, k: int) -> int:
         r=0
         while n>0:
-            r+=n%k
-            n//=k
+            if n//k:
+                r+=n%k
+                n//=k
+            else:
+                r+=n
+                n=0
         return r
