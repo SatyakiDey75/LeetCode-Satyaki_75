6624e4e5496c7b4b07bd781cd7072f73b059db62
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-26 13:42:44 +0530

    Time: 45 ms (56.99%) | Memory: 16.8 MB (69.10%) - LeetSync

--- problems/205-isomorphic-strings/isomorphic-strings.py
+++ problems/205-isomorphic-strings/isomorphic-strings.py
@@ -1,4 +1,14 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return [*map(s.index, s)] == [*map(t.index, t)]
+        if len(s)!=len(t):
+            return False
+        d={}
+        for x,y in zip(s,t):
+            if x not in d:
+                d[x]=y
+            if d[x]!=y:
+                return False
+        return len(set(d))==len(set(d.values()))
+        
+        # return [*map(s.index, s)] == [*map(t.index, t)]
         
