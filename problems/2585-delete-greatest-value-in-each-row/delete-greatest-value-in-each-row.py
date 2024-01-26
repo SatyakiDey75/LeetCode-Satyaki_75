53888439d76da512e1fa58d5232607a158ec057d
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-26 20:39:48 +0530

    Time: 62 ms (95.70%) | Memory: 11.7 MB (82.80%) - LeetSync

--- problems/2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.py
+++ problems/2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.py
@@ -1,10 +1,17 @@
 class Solution(object):
     def deleteGreatestValue(self, grid):
-        m,s=len(grid),0
-        while grid[0]!=[]:
-            l=[]
-            for i in range(m):
-                l.append(max(grid[i]))
-                grid[i].remove(max(grid[i]))
-            s+=max(l)
+        for i in grid:
+            i.sort(reverse=True)
+        s=0
+        for i in zip(*grid):
+            s+=max(i)
         return s
+
+        # m,s=len(grid),0
+        # while grid[0]!=[]:
+        #     l=[]
+        #     for i in range(m):
+        #         l.append(max(grid[i]))
+        #         grid[i].remove(max(grid[i]))
+        #     s+=max(l)
+        # return s
