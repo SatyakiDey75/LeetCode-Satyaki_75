69bb41c3b6afd32700e93e8e6e176ec420b71024
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-26 20:56:09 +0530

    Time: 13 ms (97.42%) | Memory: 11.7 MB (98.06%) - LeetSync

---
+++ problems/2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.py
@@ -0,0 +1,10 @@
+def sod(n):
+    s=0
+    while n:
+        s+=n%10
+        n//=10
+    return s
+class Solution(object):
+    def countEven(self, num):
+        return sum(1 for i in range(2,num+1) if sod(i)%2==0)
+        
