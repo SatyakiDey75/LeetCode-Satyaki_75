dd4c81afd1ce6d2677440b36fe1a4b82abdd2e76
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-26 14:23:39 +0530

    Time: 37 ms (55.56%) | Memory: 11.7 MB (97.22%) - LeetSync

--- problems/1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.py
+++ problems/1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def trimMean(self, arr):
-        rm=0.05*len(arr)
+        rm=.05*len(arr)
         arr.sort()
         s=sum(arr[int(rm):int(-1*rm)])
         return s/(len(arr)-(2*rm))
