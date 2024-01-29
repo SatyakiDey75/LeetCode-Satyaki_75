9060f6d8a695b0fd6ea763602de49fa6a07c51a1
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-29 20:54:22 +0530

    Time: 18 ms (68.97%) | Memory: 11.8 MB (95.40%) - LeetSync

--- problems/2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
+++ problems/2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -1,17 +1,10 @@
-def sod(i):
-    s=0
-    while i>0:
-        s+=i%10
-        i//=10
-    return s
-
 class Solution(object):
     def getLucky(self, s, k):
         a=""
         for i in s:
             a+=str(ord(i)-96)
-        s=int(a)
         print(a)
-        for i in range (k):
-            s=sod(s)
-        return s
+        while k>0:
+            a=sum(int(i) for i in str(a))
+            k-=1
+        return a
