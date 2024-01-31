c314d1ac7e8ae85d7dc1ed4af6b22c22fb55c384
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-31 17:14:41 +0530

    Time: 65 ms (34.68%) | Memory: 16.9 MB (52.23%) - LeetSync

---
+++ problems/692-top-k-frequent-words/top-k-frequent-words.py
@@ -0,0 +1,8 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        s=sorted(Counter(words).items(),key=lambda x:(-x[1],x[0]))
+        res=s[0:k]
+        t=dict(res)
+        
+        return ([x for x in (t.keys())])
+        
