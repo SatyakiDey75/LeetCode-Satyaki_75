6e5ada44165b0ab9611d0b5efc96bd226ec3171b
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-31 12:32:53 +0530

    Time: 25 ms (80.00%) | Memory: 11.8 MB (93.33%) - LeetSync

---
+++ problems/2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countValidWords(self, sentence):
+        def chk(i):
+            ch=0
+            for s,j in enumerate(i):
+                if j.isdigit():
+                    return False
+                if j=='-':
+                    if s==0 or not i[s-1].isalpha():
+                        return False
+                    if s==len(i)-1 or not i[s+1].isalpha():
+                        return False
+                    if ch==1:
+                        return False
+                    ch+=1
+                if j in ['!','.',',']:
+                    if s!=len(i)-1:
+                        return False
+            return True
+        return sum(chk(i) for i in sentence.split())
