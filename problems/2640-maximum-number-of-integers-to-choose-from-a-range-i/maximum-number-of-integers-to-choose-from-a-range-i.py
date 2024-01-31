e60ebd785141d89441b3d168c4269cace39090b4
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-01-31 16:57:33 +0530

    Time: 8700 ms (5.26%) | Memory: 12.7 MB (94.74%) - LeetSync

--- problems/2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
+++ problems/2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -1,13 +1,11 @@
 class Solution(object):
     def maxCount(self, banned, n, maxSum):
-        s,c,i=0,0,1
-        while i<n+1 and not s>maxSum:
-            
-            if i not in banned:
+        s,c=0,0
+        for i in range(1,n+1):
+            if i not in banned and s+i<=maxSum:
                 s+=i
-                if not s>maxSum:
-                    c+=1
-            i+=1
+                c+=1
+            elif s+i>maxSum:
+                break
         return c
-
         
