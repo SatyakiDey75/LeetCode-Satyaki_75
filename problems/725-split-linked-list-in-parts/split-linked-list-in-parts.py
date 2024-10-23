efd4de39a11449f11797d48472ed3ac189f6ce34
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-10-24 01:38:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (18.44%) - LeetSync

--- problems/725-split-linked-list-in-parts/split-linked-list-in-parts.py
+++ problems/725-split-linked-list-in-parts/split-linked-list-in-parts.py
@@ -18,21 +18,35 @@ class Solution(object):
             temp=temp.next
         n=len_lnkd // k
         r=len_lnkd % k
-        temp=head
+        arr=[None]*k
+        curr=head
         for i in range (k):
-            if temp:
-                head1=ListNode(temp.val)
-                temp1=head1
-                temp=temp.next
-                for j in range (n-1):
-                    temp1.next=ListNode(temp.val) 
-                    temp1=temp1.next
-                    temp=temp.next
-                if (len(arr)<r and len_lnkd>k) and temp:
-                    temp1.next=ListNode(temp.val)
-                    temp1=temp1.next
-                    temp=temp.next
-            else:
-                head1=None
-            arr.append(head1)
+            arr[i]=curr
+            size=n+(1 if i<r else 0)
+            for i in range (size-1):
+                curr=curr.next
+            if curr:
+                temp=curr.next
+                curr.next=None
+                curr=temp
         return arr
+
+        # for i in range (k):
+        #     if temp:
+        #         head1=ListNode(temp.val)
+        #         temp1=head1
+        #         temp=temp.next
+        #         for j in range (n-1):
+        #             temp1.next=ListNode(temp.val) 
+        #             temp1=temp1.next
+        #             temp=temp.next
+        #         if (len(arr)<r and len_lnkd>k) and temp:
+        #             temp1.next=ListNode(temp.val)
+        #             temp1=temp1.next
+        #             temp=temp.next
+        #     else:
+        #         head1=None
+        #     arr.append(head1)
+        # return arr
+
+        
