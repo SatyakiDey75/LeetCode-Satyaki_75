e41a90fbaafa16dfe21c3d3948a11d110203f1e7
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-10-31 01:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (49.85%) - LeetSync

--- problems/551-student-attendance-record-i/student-attendance-record-i.py
+++ problems/551-student-attendance-record-i/student-attendance-record-i.py
@@ -1,11 +1,37 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        if s.count('A') > 1:
-            return False
-        l = len(s)
-        if l <= 2:
-            return True
-        for i in range (len(s) - 2):
-            if s[i] == s[i+1] == s[i+2] == 'L':
-                return False
+        # if s.count('A') > 1:
+        #     return False
+        # for i in range (len(s)):
+        #     if s[i] == 'L':
+        #         try:
+        #             if s[i:i+3] == 'LLL':
+        #                 return False 
+        #         except:
+        #             continue
+        # return True
+
+        # if s.count('A') > 1:
+        #     return False
+        # l = len(s)
+        # if l <= 2:
+        #     return True
+        # for i in range (len(s) - 2):
+        #     if s[i] == s[i+1] == s[i+2] == 'L':
+        #         return False
+        # return True
+
+        A, L, i = 0, 0, 0
+        while i < len(s):
+            if s[i] == "A": 
+                A += 1
+                if A >= 2:
+                    return False
+            if s[i] == "L":
+                L += 1
+                if L >= 3:
+                    return False
+            else:
+                L = 0
+            i += 1
         return True
