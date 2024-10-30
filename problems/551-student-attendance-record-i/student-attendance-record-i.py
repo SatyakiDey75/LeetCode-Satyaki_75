de83c8d875b1300f7d4464abd794749cde8ee078
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-10-31 01:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (13.10%) - LeetSync

--- problems/551-student-attendance-record-i/student-attendance-record-i.py
+++ problems/551-student-attendance-record-i/student-attendance-record-i.py
@@ -2,11 +2,10 @@ class Solution:
     def checkRecord(self, s: str) -> bool:
         if s.count('A') > 1:
             return False
-        for i in range (len(s)):
-            if s[i] == 'L':
-                try:
-                    if s[i:i+3] == 'LLL':
-                        return False 
-                except:
-                    continue
+        l = len(s)
+        if l <= 2:
+            return True
+        for i in range (len(s) - 2):
+            if s[i] == s[i+1] == s[i+2] == 'L':
+                return False
         return True
