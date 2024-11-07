9635a413af2aac7d8fdfb004282282a02d20049a
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-11-08 00:09:32 +0530

    Time: 1891 ms (38.95%) | Memory: 55.5 MB (38.39%) - LeetSync

--- problems/204-count-primes/count-primes.py
+++ problems/204-count-primes/count-primes.py
@@ -1,14 +1,29 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        pr = [True for i in range (n+1)]
-        i = 2
-        while (i*i <= n):
-            if pr[i] == True:
-                for j in range (i*i, n+1, i):
-                    pr[j] = False
-            i += 1
-        ans = 0
-        for i in range(2, n):
-            if pr[i]:
-                ans += 1
-        return ans
+        # pr = [True for i in range (n+1)]
+        # i = 2
+        # while (i*i <= n):
+        #     if pr[i] == True:
+        #         for j in range (i*i, n+1, i):
+        #             pr[j] = False
+        #     i += 1
+        # ans = 0
+        # for i in range(2, n):
+        #     if pr[i]:
+        #         ans += 1
+        # return ans
+        if n <= 1:
+            return 0
+        
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        cnt = 1
+
+        for i in range(2, int(n**0.5) + 1):
+            if primes[i]:
+                for j in range(i*i, n, i):
+                    if primes[j]:
+                        cnt += 1
+                    primes[j] = False
+
+        return n - cnt - 1
