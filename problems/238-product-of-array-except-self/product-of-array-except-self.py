c4ee9aca902fa31346f7b668449aeda098cd1bbc
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2024-11-07 23:05:38 +0530

    Time: 14 ms (97.78%) | Memory: 22.2 MB (98.84%) - LeetSync

--- problems/238-product-of-array-except-self/product-of-array-except-self.py
+++ problems/238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,37 +1,37 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # if nums.count(0) > 1:
-        #     return [0 for i in nums]
-        # s = 1
-        # for i in nums:
-        #     if i != 0:
-        #         s *= i
+        if nums.count(0) > 1:
+            return [0 for i in nums]
+        s = 1
+        for i in nums:
+            if i != 0:
+                s *= i
         
-        # ans = []
-        # if 0 in nums:
-        #     for i in nums:
-        #         if i != 0:
-        #             ans.append(0)
-        #         else:
-        #             ans.append(s)
-        #     return ans
-        # for i in nums:
-        #     if i != 0:
-        #         ans.append(s // i)
-        #     else:
-        #         ans.append(s)
-        # return ans
+        ans = []
+        if 0 in nums:
+            for i in nums:
+                if i != 0:
+                    ans.append(0)
+                else:
+                    ans.append(s)
+            return ans
+        for i in nums:
+            if i != 0:
+                ans.append(s // i)
+            else:
+                ans.append(s)
+        return ans
 
-        ans = [1] * len(nums)
+        # ans = [1] * len(nums)
         
-        l = 1
-        for i in range(len(nums)):
-            ans[i] *= l
-            l *= nums[i]
+        # l = 1
+        # for i in range(len(nums)):
+        #     ans[i] *= l
+        #     l *= nums[i]
         
-        r = 1
-        for i in range(len(nums) - 1, -1, -1):
-            ans[i] *= r
-            r *= nums[i]
+        # r = 1
+        # for i in range(len(nums) - 1, -1, -1):
+        #     ans[i] *= r
+        #     r *= nums[i]
     
-        return ans  
+        # return ans  
