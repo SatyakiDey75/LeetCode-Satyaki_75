7570eda99fbd533ae04897dcb81053448b6faa08
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2025-07-08 10:38:56 +0530

    Time: 55 ms (5.00%) | Memory: 17.9 MB (33.00%) - LeetSync

--- problems/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ problems/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -2,7 +2,6 @@ class Solution:
     def findLucky(self, arr: List[int]) -> int:
         res = -1
         for i in arr:
-            if arr.count(i) == i:
-              if  i > res:
+            if arr.count(i) == i and  i > res:
                 res = i
         return res
