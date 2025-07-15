cb36c16a1aa8af48e7995dc38b11f05b730b4b1a
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2025-07-15 19:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (34.46%) - LeetSync

--- problems/3396-valid-word/valid-word.py
+++ problems/3396-valid-word/valid-word.py
@@ -1,32 +1,33 @@
 class Solution:
     def isValid(self, word: str) -> bool:
-        if len(word) < 3:
+        # if len(word) < 3:
+        #     return False
+        # vowels = 'aeiouAEIOU'
+
+        # def isConsonant(c: str) -> bool:
+        #     return c.isalpha() and c not in vowels
+
+        # con, vow = 0, 0
+        # for c in word:
+        #     if not c.isalnum():
+        #         return False
+        #     if isConsonant(c):
+        #         con += 1
+        #     if c in vowels:
+        #         vow += 1
+        # if con == 0 or vow == 0:
+        #     return False
+        # return True
+
+        if len(word) < 3: 
             return False
-        vowels = 'aeiouAEIOU'
-
-        def isConsonant(c: str) -> bool:
-            return c.isalpha() and c not in vowels
-
-        con, vow = 0, 0
-        for c in word:
-            if not c.isalnum():
+        v, c = False, False
+        for x in word:
+            if not x.isdigit() and not x.isalpha(): 
                 return False
-            if isConsonant(c):
-                con += 1
-            if c in vowels:
-                vow += 1
-        if con == 0 or vow == 0:
-            return False
-        return True
+            if x.lower() in 'aeiou':
+                v = True
+            elif not x.isdigit():
+                c = True
 
-# class Solution:
-#   def isValid(self, word: str) -> bool:
-#     VOWELS = 'aeiouAEIOU'
-
-#     def isConsonant(c: str) -> bool:
-#       return c.isalpha() and c not in VOWELS
-
-#     return (len(word) >= 3 and
-#             all(c.isalnum() for c in word) and
-#             any(c in VOWELS for c in word) and
-#             any(isConsonant(c) for c in word))
+        return v and c
