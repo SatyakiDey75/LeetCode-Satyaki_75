f46ce64b38fcdcd259fb3f1416c87fde2f34549c
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2025-07-15 19:09:24 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (9.76%) - LeetSync

---
+++ problems/3396-valid-word/valid-word.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3:
+            return False
+        vowels = 'aeiouAEIOU'
+
+        def isConsonant(c: str) -> bool:
+            return c.isalpha() and c not in vowels
+
+        con, vow = 0, 0
+        for c in word:
+            if not c.isalnum():
+                return False
+            if isConsonant(c):
+                con += 1
+            if c in vowels:
+                vow += 1
+        if con == 0 or vow == 0:
+            return False
+        return True
